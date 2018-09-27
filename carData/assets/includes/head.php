83be364b206a26bdf1fcd5528114136e2c0e7229
Parker George <parkergeorge30@yahoo.com>
2018-09-27 10:33:28 -0700

    first commit

---
+++ carData/assets/includes/head.php
@@ -0,0 +1,14 @@
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Good Morning, Lets Get This Bread</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="templates/startbootstrap-modern-business-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="modern-business.css" rel="stylesheet">
+</head>
---
+++ carData/assets/includes/nav.php
@@ -0,0 +1,30 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="/student/globalit/2018/04_06/Tinker/carData/index.php">Car Site</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="templates/startbootstrap-modern-business-gh-pages/about.html">Search</a>
+			</li>
+            <li class="nav-item">
+              <a class="nav-link" href="templates/startbootstrap-modern-business-gh-pages/contact.html">Contact</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Type
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="car.php">Vehicle</a>
+                <a class="dropdown-item" href="templates/startbootstrap-modern-business-gh-pages/portfolio-2-col.html">2 Column Portfolio</a>
+                <a class="dropdown-item" href="templates/startbootstrap-modern-business-gh-pages/portfolio-3-col.html">3 Column Portfolio</a>
+                <a class="dropdown-item" href="templates/startbootstrap-modern-business-gh-pages/portfolio-4-col.html">4 Column Portfolio</a>
+                <a class="dropdown-item" href="category.php">info</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
